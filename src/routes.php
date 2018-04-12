9e5db80f27906a392540f25f01d8076f7b64c5db
lueking <lueking@csh.rit.edu>
2018-04-12 18:57:59 -0400

    derp

--- src/routes.php
+++ src/routes.php
@@ -4,29 +4,30 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
-$app->group('', function () use ($app) {
 
-    $auth = new \WebDrink\Middleware\AuthMiddleware();
 
-    //User route, this is the normal view
-    $app->get('/', function (Request $request, Response $response, array $args){
-        return $response->withJson('/');
-    })->add($auth);
+$auth = new \WebDrink\Middleware\AuthMiddleware();
 
-    $app->get('/auth', function (Request $request, Response $response, array $args){
-        return $response->withJson('/auth');
-    })->add($auth);
+$app->get('/auth', function (Request $request, Response $response, array $args){
+    return $response->withJson("snort");
+})->add($auth);
+
+//User route, this is the normal view
+$app->get('/', function (Request $request, Response $response, array $args){
+    return $response->withJson("bork");
+})->add($auth);
+
+
 
 
 //Where to go to get an API key
-    $app->get('/settings', function (Request $request, Response $response, array $args) {
+$app->get('/settings', function (Request $request, Response $response, array $args) {
 
-    });
+});
 
 //Secret drink admin things
-    $app->get('/admin', function (Request $request, Response $response, array $args) {
+$app->get('/admin', function (Request $request, Response $response, array $args) {
 
-    });
 });
 
 
