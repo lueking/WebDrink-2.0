96985cada3b5d4c8346329d070c51988ead8e577
lueking <lueking@csh.rit.edu>
2018-04-12 16:41:45 -0400

    derp

--- src/routes.php
+++ src/routes.php
@@ -6,23 +6,23 @@ use Slim\Http\Response;
 
 // Routes
 
-$auth = new \WebDrink\Middleware\KeycloakMiddleware();
+
 
 //User route, this is the normal view
 $app->get('/', function (Request $request, Response $response, array $args) {
-    $auth = $request->getAttribute('auth');
+    $auth = new \WebDrink\Middleware\KeycloakMiddleware();
 
     return $response->withJson(var_export($auth));
     //return $this->renderer->render($response, 'index.twig', $args);
-})->add($auth);
+});
 
 //Where to go to get an API key
 $app->get('/settings', function (Request $request, Response $response, array $args) {
 
-})->add($auth);
+});
 
 //Secret drink admin things
 $app->get('/admin', function (Request $request, Response $response, array $args) {
 
-})->add($auth);
+});
 
