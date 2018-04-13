2dc72a58d0f1df9a2488cef0b7df085725b1d3ef
lueking <lueking@csh.rit.edu>
2018-04-13 01:18:18 -0400

    derp

--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -4,17 +4,43 @@ namespace WebDrink\Middleware;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Stevenmaguire\OAuth2\Client\Provider\Keycloak;
 use WebDrink\Utils\OIDC;
 
 class AuthMiddleware {
+
+    private $token;
+    private $provider;
+
+    public function __construct() {
+        $this->provider = new Keycloak([
+            'authServerUrl' => OIDC_PROVIDER_URL,
+            'realm' => OIDC_PROVIDER_REALM,
+            'clientId' => OIDC_CLIENT_ID,
+            'clientSecret' => OIDC_CLIENT_SECRET,
+            'scope' => 'openid',
+            'redirectUri' => 'https://webdrink-dev.csh.rit.edu/auth'
+        ]);
+    }
+
     public function __invoke(Request $request, Response $response, $next) {
-            // Makes route Require Auth
-            $oidc = new OIDC();
-            $auth = $oidc->getAuth();
-            $auth->authenticate();
 
-            $request = $request->withAttribute('auth', $auth);
+        //first step is making sure we have a code
 
-            return $next($request, $response);
+
+        if(!is_null($this->token)){
+            $this->provider->authorize();
+        }
+
+
+
+        $request = $request->withAttribute('auth', $auth);
+
+        return $next($request, $response);
+    }
+
+
+    function (){
+
     }
 }
