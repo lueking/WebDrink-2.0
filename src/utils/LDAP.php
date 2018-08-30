a495246380d7d4f76af52a947fdf94800ee67797
lueking <lueking@csh.rit.edu>
2018-08-29 21:01:52 -0400

    I just spent hours diagnosing a non-issue, fml

--- src/utils/LDAP.php
+++ src/utils/LDAP.php
@@ -6,66 +6,59 @@ namespace WebDrink\Utils;
 use Exception;
 
 class LDAP {
-    private $userDn;
-    private $conn;
+    private $userDn = "cn=users,cn=accounts,dc=csh,dc=rit,dc=edu";
+    private $appDn = "cn=services,cn=accounts,dc=csh,dc=rit,dc=edu";
+    private $conn = null;
+
 
     public function __construct() {
         // LDAP connection info
         $ldapUser = LDAP_USER;
         $ldapPass = LDAP_PASS;
         $ldapHost = LDAP_HOST;
-        $appDn = "cn=services,cn=accounts,dc=csh,dc=rit,dc=edu";
-        $this->userDn = "cn=users,cn=accounts,dc=csh,dc=rit,dc=edu";
 
         // Append the appropriate dn to the username
         if (LDAP_APP) {
-            $ldapUser .= "," . $appDn;
+            $ldapUser .= "," . $this->appDn;
         } else {
             $ldapUser .= "," . $this->userDn;
         }
 
         // Connect to LDAP and bind the connection
-        try {
-            $this->conn = ldap_connect($ldapHost);
-            if (!ldap_bind($this->conn, $ldapUser, $ldapPass)) {
-                die ('LDAP Bind Error...');
-            }
-        }
-        catch (Exception $e) {
-            die ('LDAP Connection Failed: ' . $e->getMessage());
+        $conn = ldap_connect($ldapHost);
+        if (!ldap_bind($conn, $ldapUser, $ldapPass)) {
+            print("LDAP ERROr!");
+            print(ldap_err2str(ldap_errno($conn)));
+            throw new Exception("BAAAD");
+        } else {
+            $this->conn = $conn;
+            print("Successful LDAP Connection");
         }
     }
 
+    private function getconn(){
+        if(is_null($this->conn)){
+            throw new Exception("Error, no connection to LDAP");
+        }
+
+        return $this->conn;
+    }
+
     function ldap_lookup($uid, $fields = null) {
-        try {
             // Make the search
             $filter = "(uid=" . $uid . ")";
-            print ("\nconn:");
-            print (var_export($this->conn));
-
-            print ("\nuserDn:");
-            print (var_export($this->userDn) );
-
-            print ("\nfilter:");
-            print (var_export($filter) );
-
-            print ("\nfields:");
-            print (var_export($fields) );
 
             if (is_array($fields)) {
-                $search = ldap_search($this->conn, $this->userDn, $filter, $fields);
+                $search = ldap_search($this->getconn(), $this->userDn, $filter, $fields);
 
-            }
-            else
-                $search = ldap_search($this->conn, $this->userDn, $filter);
+            } else
+                $search = ldap_search($this->getconn(), $this->userDn, $filter);
             // Grab the results
             if ($search)
-                return ldap_get_entries($this->conn, $search);
+                return ldap_get_entries($this->getconn(), $search);
             else
                 return false;
-        } catch (Exception $e) {
-            return false;
-        }
+
     }
 
     function ldap_lookup_uid($uid, $fields = null) {
